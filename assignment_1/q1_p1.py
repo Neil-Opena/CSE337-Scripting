39ab5669f420dedb14ca5e7ea77580fde9341e1e
Neil Opena <neil1114o@gmail.com>
2018-09-22 17:41:02 -0400

    Finished part 1 of question 1

--- assignment_1/q1_p1.py
+++ assignment_1/q1_p1.py
@@ -2,29 +2,28 @@
 #Part 1
 import time
 price_file = open("prices_sample.csv")
-date_dictionary = {} #day = key, value = time
+my_dictionary = {} #day = key, value = time
 
-for line in price_file:
-    file_line = price_file.readline()
+for file_line in price_file:
     line_list = file_line.split(",")
-    epoch_date = line_list[0]
+    epoch_date = line_list[0].strip()
+    price = line_list[1].strip()
+
     formatted_date_time = (time.strftime("%Y-%m-%d %H:%M:%S",(time.gmtime(int(epoch_date))))).split(" ") #YYYY-MM-DD HH:MM:SS
-
     day = formatted_date_time[0]
-    day_time = formatted_date_time[1]
 
-    day_time_list = date_dictionary.get(day)
-    if(day_time_list == None):
-        date_dictionary[day] = [day_time]
+    prices_list = my_dictionary.get(day)
+    if(prices_list == None):
+        my_dictionary[day] = [price]
     else:
-        day_time_list.append(day_time)
+        prices_list.append(price)
 
 price_file.close()
 
 output_file = open("q1_p1_output.txt", "w")
 
-for key in date_dictionary.keys():
-    values = date_dictionary.get(key)
+for key in my_dictionary.keys():
+    values = my_dictionary.get(key)
     output_file.write(key + ": ")
     output_file.write(",".join(values))
     output_file.write("\n")
