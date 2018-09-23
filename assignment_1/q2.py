12e877faf11024b95e63bf962ecbc25220e2a92f
Neil Opena <neil1114o@gmail.com>
2018-09-23 18:51:10 -0400

    Added date time module for question 2

--- assignment_1/q2.py
+++ assignment_1/q2.py
@@ -1,1 +1,17 @@
 #Daily Data
+import time
+import datetime
+date_price_file = open("q1_p1_output.txt")
+
+weekly_prices = {}
+
+for day_price_line in date_price_file:
+    temp_str = day_price_line.split(":")
+    day = temp_str[0]
+    formatted_time = datetime.datetime.strptime(day, "%Y-%m-%d")
+    # if(formatted_time.tm_wday == 0): # wday = week day, Monday = 0
+    #     weekly_prices[day] = list(map(int, temp_str[1].strip().split(",")))
+    #     #else
+    #     # find the corresponding key that corresponds to the day of the week
+    print(formatted_time)
+    
