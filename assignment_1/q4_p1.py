ec0b541cfb6fc4e46079554dc6494921eeddd6d6
Neil Opena <neil1114o@gmail.com>
2018-09-28 18:49:52 -0400

    Basic implementation of q4 p1 completed

--- assignment_1/q4_p1.py
+++ assignment_1/q4_p1.py
@@ -1,18 +1,33 @@
 #Word Analytics
 #Part 1
 
-import sys
-
-if(len(sys.argv) != 3):
-    print("put text here.... args..")
-else:
-    file_name = sys.argv[1] #first argument = file name
-    n = sys.argv[2] #second argument = n
-    print("file-to-open",file_name)
-    print("n",n)
-
-
 #get all of the file text, and separate by spaces
 #for windows of n, put into dictionary, key = n-gram, value = count?
+
+def get_n_gram_dictionary(file_name, n):
+
+    dictionary = {}
+
+    f = open(file_name)
+
+    f_contents = f.read().strip()
+    f_word_list = f_contents.split(" ")
+
+    for i in range(0, len(f_word_list) - (n - 1)):
+        n_gram = " ".join(f_word_list[i:n + i])
+        num_occurrences = dictionary.get(n_gram)
+        if(num_occurrences == None):
+            dictionary[n_gram] = 1
+        else:
+            num_occurrences += 1
+            dictionary[n_gram] = num_occurrences
+
+    f.close()
+
+
+    return dictionary
+
+
+print(get_n_gram_dictionary("q4_test_file.txt", 3))
 
 
