ae563dce78dfdeea28ef6fc5b60c028115a44c71
Neil Opena <neil.opena97@gmail.com>
2018-10-27 14:47:20 -0400

    Basic implementation of q2 p2 completed

---
+++ assignment_2/q2_p2.pl
@@ -0,0 +1,39 @@
+#Part 2 - Text Transformation
+
+open INPUT, "<q2.in" or die "Can't open input file: $!";
+open OUTPUT, ">q2p2.out" or die "Can't open output file: $!";
+
+my $k = <STDIN>;
+chomp $k;
+
+my @original = ();
+my @output = ();
+
+while(<INPUT>){
+    if(keep_line($_, $k)){
+	push (@output, $_);
+    }
+    push (@original, $_);
+}
+
+if(scalar @output == scalar @original){
+    print OUTPUT "Oooh Nooo!";
+}else{
+    foreach my $line (@output){
+        print OUTPUT $line;
+    }
+}
+
+sub keep_line{
+    my $line = @_[0];
+
+    my @words = split(" ", @_[0]);
+    if(scalar @words == @_[1]){
+    	return 0;
+    }else{
+    	return 1; #1 == true, 0 == false
+    }
+}
+
+close INTPUT;
+close OUTPUT;
