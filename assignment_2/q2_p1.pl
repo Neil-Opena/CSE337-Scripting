69bda07725445e91dff62e7d2435b3a144c18a8a
Neil Opena <neil.opena97@gmail.com>
2018-10-27 14:16:07 -0400

    Basic implementation of q2 p1 completed

--- assignment_2/q2_p1.pl
+++ assignment_2/q2_p1.pl
@@ -3,11 +3,30 @@
 open INPUT, "<q2.in" or die "Can't open input file: $!";
 open OUTPUT, ">q2p1.out" or die "Can't open output file: $!";
 
+#Get standard input str1, str2
+my $str1 = <STDIN>;
+chomp $str1;
+
+my $str2 = <STDIN>;
+chomp $str2;
+
 while(<INPUT>){
-    print $_;
+    my $result = swap_line($_, $str1, $str2);
+    print OUTPUT "$result";
 }
 
-# sub replace_line()
+sub swap_line{
+    #first arg = sentence
+    #second arg = word to replace
+    #third arg = replacement word
+    #my @line = split("", @_[0]);
+    my $line = @_[0];
+    my $str1 = @_[1];
+    my $str2 = @_[2];
+
+    $line =~ s/$str1/$str2/g;
+    return $line;
+}
 
 close INPUT;
 close OUTPUT;
