690bf4490263a73a25bc7e078e506c1a8cc9a616
Neil Opena <neil1114o@gmail.com>
2018-10-28 11:00:27 -0400

    Finished basic implementation of q3 p2

--- assignment_2/q3_p2.pl
+++ assignment_2/q3_p2.pl
@@ -2,40 +2,56 @@
 use strict;
 use warnings;
 
-my %table = ("JAN"=>4840, "FEB"=>4340, "MAR"=>3900, "APR"=>4330, "MAY"=>3090, "JUN"=>3660, "JUL"=>3520, "AUG"=>3280, "SEP"=>4130, "OCT"=>3690, "NOV"=>4260, "DEC"=>4800);
+# my %table = ("jan"=>4840, "feb"=>4340, "mar"=>3900, "apr"=>4330, "may"=>3090, "jun"=>3660, "jul"=>3520, "aug"=>3280, "sep"=>4130, "oct"=>3690, "nov"=>4260, "dec"=>4800);
+my %cummulative_table = ("jan"=>4840, "feb"=>9180, "mar"=>13080, "apr"=>17410, "may"=>20500, "jun"=>24160, "jul"=>27680, "aug"=>30960, "sep"=>35090, "oct"=>38780, "nov"=>43040, "dec"=>47840);
+my %to_subtract = ("jan"=>0, "feb"=>4840, "mar"=>9180, "apr"=>13080, "may"=>17410, "jun"=>20500, "jul"=>24160, "aug"=>27680, "sep"=>30960, "oct"=>35090, "nov"=>38780, "dec"=>43040);
 
+print "Months can be selected using the three initials letters.\n";
+my $three_initial_phrase = "Please enter only the three initials of a valid month.";
 
 print "Enter the initial month: ";
 my $initial = <STDIN>;
 chomp $initial;
-$initial = uc $initial;
+$initial = lc $initial;
 
 until(valid_key($initial)){
+    print "$three_initial_phrase\n";
     print "Re-enter the initial month: ";
     $initial = <STDIN>;
     chomp $initial;
-    $initial = uc $initial;
+    $initial = lc $initial;
 }
 
 print "Enter the final month: ";
 my $final = <STDIN>;
 chomp $final;
-$final = uc $final;
+$final = lc $final;
 
-until(valid_key($initial)){
+until(valid_final($final)){
+    print "$three_initial_phrase\n";
     print "Re-enter the final month: ";
-    $initial = <STDIN>;
-    chomp $initial;
-    $initial = uc $initial;
+    $final = <STDIN>;
+    chomp $final;
+    $final = lc $final;
 }
 
+my $ans = $cummulative_table{$final} - $to_subtract{$initial};
+print "The cumulative revenue is: $ans";
 
 sub valid_key{
     my $test = $_[0];
-    foreach my $month (keys %table){
+    foreach my $month (keys %cummulative_table){
         if($month eq $test){
-	    return 1;
-	}
+	        return 1;
+	    }
     }
     return 0;
+}
+
+sub valid_final{
+    my $to_return = valid_key($_[0]);
+    if($cummulative_table{$_[0]} < $cummulative_table{$initial}){
+        return 0;
+    }
+    return $to_return;
 }
