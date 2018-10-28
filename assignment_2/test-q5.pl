a51afeefc8108b378a50557e6d3a6dfc882ab4ca
Neil Opena <neil1114o@gmail.com>
2018-10-28 14:23:48 -0400

    Finished basic implementation of q5 p2

--- assignment_2/test-q5.pl
+++ assignment_2/test-q5.pl
@@ -1,8 +1,8 @@
 #Test for Part 5
 my @strings_1 = ("be or not to be", "to be to be", "to be to be to be", "to be to be to be to be", "to be or", "to be", "to be or not", "to be or not to");
 my @strings_2 = ("hello", "world!!", "hello!?", "perl?!", "cat.", "dog?", "computer..", "world!");
-my @strings_3 = ("1/25/2018", "3/11/2119", "6/8/224", "6/54/1996");
-my @strings_4 = ("1234 aaaa bbbb", "cat dog boy", "truck", "cat ", "12 12 ");
+my @strings_3 = ("1/25/2018", "3/11/2119", "6/8/224", "1/30/0","1/1/131357");;
+my @strings_4 = ("bag ok asdfasdfafdas", "o aaasasdfas a", "okasdfasfdsaf lol a", "abc a asdfasfdasfasfa", "doga a al", "asb asdf llll");
 
 # foreach my $string (@strings_1){
     # if($string =~ m/((to|be|or|not)\s)+(to be)$/){
@@ -19,3 +19,19 @@ my @strings_4 = ("1234 aaaa bbbb", "cat dog boy", "truck", "cat ", "12 12 ");
         # print "false\n";
     # }
 # }
+
+# foreach my $string (@strings_3){
+    # if($string =~ m/^([1-9]|1[0-2])\/([1-9]|[12]\d|30)\/[1-9]\d{0,3}$/){
+        # print "true\n";
+    # }else{
+        # print "false\n";
+    # }
+# }
+
+foreach my $string (@strings_4){
+    if($string =~ m/^((\w{1,4})\s)*\w{1,4}$/){
+        print "true\n";
+    }else{
+        print "false\n";
+    }
+}
