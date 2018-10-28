5ca830f3c256d0fc9f8d860121ef248785c1ec1e
Neil Opena <neil1114o@gmail.com>
2018-10-28 13:58:09 -0400

    Basic implementation of q5 p1 completed

---
+++ assignment_2/test-q5.pl
@@ -0,0 +1,21 @@
+#Test for Part 5
+my @strings_1 = ("be or not to be", "to be to be", "to be to be to be", "to be to be to be to be", "to be or", "to be", "to be or not", "to be or not to");
+my @strings_2 = ("hello", "world!!", "hello!?", "perl?!", "cat.", "dog?", "computer..", "world!");
+my @strings_3 = ("1/25/2018", "3/11/2119", "6/8/224", "6/54/1996");
+my @strings_4 = ("1234 aaaa bbbb", "cat dog boy", "truck", "cat ", "12 12 ");
+
+# foreach my $string (@strings_1){
+    # if($string =~ m/((to|be|or|not)\s)+(to be)$/){
+        # print "true\n";
+    # }else{
+        # print "false\n";
+    # }
+# }
+
+# foreach my $string (@strings_2){
+    # if($string =~ m/(\w|[!?]{2})$/){
+        # print "true\n";
+    # }else{
+        # print "false\n";
+    # }
+# }
