6c0ff41720a4783ff4c8f723d7fd22a26d677500
Neil Opena <neil.opena97@gmail.com>
2018-11-04 16:24:29 -0500

    Added bfs and optimization

--- CSE373/hw4.py
+++ CSE373/hw4.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import queue
 class EdgeNode():
     def __init__(self, y, next_edge=None):
         self.y = y
@@ -17,9 +18,13 @@ class Graph():
 
         self.finished = False
         self.setup_completed = False
-        self.max_degree = 0
+        self.ordered_vertices = []
         self.min_degree = 0
-        self.solution = 0
+        self.max_degree = 0
+        self.lower_bound = 0
+
+        self.heuristic = []
+        self.solution = []
         self.solution_length = 0
 
     def read_graph(self, file_name):
@@ -35,30 +40,48 @@ class Graph():
             y = int(edge[1])
             self.insert_edge(x, y, False)
 
-        self.min_degree = self.get_min_degree()
         self.solution_length = self.num_vertices
 
-        f.close()
+        self.ordered_vertices = [i[0] + 1 for i in sorted(enumerate(self.degree_list), key = lambda x:x[1])]
+        self.min_degree = self.degree_list[self.ordered_vertices[0] - 1]
+        self.max_degree = self.degree_list[self.ordered_vertices[-1] - 1]
+        self.lower_bound = math.ceil(self.max_degree / 2)
 
-    def get_min_degree(self):
-        minimum = self.num_vertices
-        for i in range(0, len(self.degree_list)):
-            if(self.degree_list[i] < minimum):
-                minimum = self.degree_list[i]
-        return minimum
+        # perform bfs with vertex with smallest degree
+
+        for i in range(0, self.num_vertices):
+            if(i + 1 not in self.heuristic):
+                self.heuristic += self.bfs(self.ordered_vertices[i])
+
+        f.close()
 
     def insert_edge(self, x, y, is_directed):
         edge_node = EdgeNode(y, self.adjacency_list[x - 1])
         self.adjacency_list[x - 1] = edge_node
         self.degree_list[x - 1] += 1
-        if(self.degree_list[x - 1] > self.max_degree):
-            self.max_degree = self.degree_list[x - 1]
-
         if(is_directed == False):
             self.insert_edge(y, x, True)
 
+    def bfs(self, start):
+        q = queue.Queue()
+        discovered = set()
+        discovered.add(start)
+        q.put(start)
+        result = []
+        while(not q.empty()):
+            v = q.get()
+            result.append(v)
+            edge = self.adjacency_list[v - 1]
+            while(edge != None):
+                y = edge.y
+                if(y not in discovered):
+                    q.put(y)
+                    discovered.add(y)
+                edge = edge.next
+        return result
+
     def solve_bandwidth(self):
-        a = [0]*self.num_vertices #solution list
+        a = [-1]*self.num_vertices #solution list
         k = 0
         self.finished = False
         self.permute_vertices(a, k)
@@ -80,25 +103,34 @@ class Graph():
                 a[k-1] = -1
 
     def continue_from_prune(self, a, k):
-        if(self.degree_list[a[0] - 1] == self.min_degree): #first vertex should have minimum degree
+        if(self.get_bandwidth(a,k) < self.solution_length): # check if edges in the graph are higher than lower bound already
             return True
-
-        # check if edges in the graph are higher than lower bound already
-        # FIXME
+        print(a, "bandwidth=", self.get_bandwidth(a,k))
+        return False
 
         # what if theres a check for symmetry
-        return False
+
+    def get_bandwidth(self, a, k):
+        max_length = 0
+        for i in range(0, k):
+            # for each vertex in the solution vector, get the bandwidth
+            v = a[i]
+            edge = self.adjacency_list[v - 1]
+            while(edge != None):
+                temp_length = 0
+                for j in range(0, k):
+                    if(a[j] == edge.y):
+                        temp_length = j - i
+                        break
+                if(temp_length != 0 and temp_length > max_length):
+                    max_length = temp_length
+                edge = edge.next
+        return max_length
 
     def generate_candidates(self, a, k, c):
         # get numbers not in the array yet
-        # generate solution through bfs
-        # make method to generate solution as a graph variable
-        #b = [6,3,7,1,2,4,5,8,9,10] # bt-10-9
-        #b = [1,5,6,4,7,2,10,8,9,3] # p-10-9
-        #b = [1,4,8,9,2,3,6,5,7,10,11] # t-11-10
-        b = [3,4,14,9,15,21,11,24,18,20,25,7,23,19,10,1,8,12,5,6,17,2,22,16,13]
         if(not self.setup_completed):
-            for i in range(b[k - 1], self.num_vertices + 1): # go through possible vertices
+            for i in range(self.heuristic[k - 1], self.num_vertices + 1): # go through possible vertices
                 found = False
                 for j in range(0, k): # check solution vector for vertices placed
                     if a[j] == (i):
@@ -114,8 +146,6 @@ class Graph():
                 if(not found):
                     c.append(i)
 
-
-
             # MAKE METHOD TO CHECK IF GREATER
 
     def check_permutation(self, a):
@@ -126,7 +156,6 @@ class Graph():
             edge = self.adjacency_list[i]
             while(edge != None):
                 # get distance from vertex (i + 1) and edge in permutation
-                # revise later
                 if(i+1 < edge.y): # only check half
                     start = -1
                     end = -1
@@ -142,8 +171,8 @@ class Graph():
 
         if(max_length < self.solution_length):
             self.solution_length = max_length
-            self.solution = a
-            if(max_length <= math.ceil(self.max_degree / 2)): # if it hits the lower bound, stop
+            self.solution = list(a)
+            if(max_length <= self.lower_bound): # if it hits the lower bound, stop
                 self.finished = True
         print(a, "=>", max_length)
         # find the longest edge based on permutation
@@ -153,4 +182,3 @@ file_name = sys.argv[1]
 graph = Graph()
 graph.read_graph(file_name)
 graph.solve_bandwidth()
-
