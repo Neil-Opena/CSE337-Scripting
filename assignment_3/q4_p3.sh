5a62d296f9c0914b2f8d4e3873c1c307aae6ee28
Neil Opena <neil.opena97@gmail.com>
2018-11-25 13:19:29 -0500

    Finished q4_p3

--- assignment_3/q4_p3.sh
+++ assignment_3/q4_p3.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+# Part 3
+echo -n "Enter a sentence: "
+read sentence
+
+count=0
+
+while read -n1 char; do
+	if [ "$char" == "k" ]; then
+		((count++))
+	fi
+done < <(echo -n $sentence)
+
+echo "$count"
