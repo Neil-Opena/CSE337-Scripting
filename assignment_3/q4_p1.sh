ffd77eecb1ddd9799502d7dc3491f01768c09f62
Neil Opena <neil.opena97@gmail.com>
2018-11-25 14:13:48 -0500

    Finished q4_p1

--- assignment_3/q4_p1.sh
+++ assignment_3/q4_p1.sh
@@ -2,3 +2,21 @@
 # Part 1
 
 # command line argument = directory dir
+dir=$1
+echo "Current shell is: $SHELL"
+echo "Current directory is: $PWD"
+echo "Home directory is: $HOME"
+echo ""
+echo "--5 most recently modified non-empty subdirectories--"
+# 5 most recently modified non-empty subdirectories in the current directory in long listing format
+ls -clt $dir | head -6 | tail -5
+echo ""
+echo "--Files in last 45 minutes"
+# the files in the current directory that are modified less than 45 minutes ago with a size of at least 1000 bytes
+find $dir -cmin -45 -size +1000b
+echo ""
+# line of 70 equal signs
+for i in `seq 1 70`;
+do
+	echo -n "="
+done
