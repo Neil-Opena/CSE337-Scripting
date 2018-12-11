31ab0de3a669b6ca67ec8ad8c64508c4bf999a0f
Neil Opena <neil.opena97@gmail.com>
2018-12-11 11:04:42 -0500

    Created guis

--- assignment_4/q3/q3.py
+++ assignment_4/q3/q3.py
@@ -33,15 +33,42 @@ def show_edit():
     address_entry.grid(row=2, column=1, columnspan=3)
     personal_radio.grid(row=3, column=1)
     business_radio.grid(row=3, column=2)
-    save_button.grid(row=4, column=1, columnspan=2)
+    # save_button.grid(row=4, column=1, columnspan=2)
+    save_button.grid(row=4, column=0, columnspan=4)
 
 def show_search():
     search_window = Toplevel(root)
 
+    name_label = Label(search_window, text="Name")
+    name_entry = Entry(search_window, width=40)
+    search_result = Label(search_window, width=40, height=5, text="")
+
+    def search_record():
+        print("search record")
+        search_result.config(text="bet")
+
+    search_button = Button(search_window, text="Search record", command=search_record)
+
+    name_label.grid(row=0, column=0)
+    name_entry.grid(row=0, column=1, columnspan=3)
+    search_result.grid(row=1, column=0, columnspan=4)
+    search_button.grid(row=2, column=0, columnspan=4)
+
 def show_delete():
     delete_window = Toplevel(root)
 
-choice = IntVar()
+    name_label = Label(delete_window, text="Name")
+    name_entry = Entry(delete_window, width=40)
+
+    def delete_record():
+        print("delete record")
+
+    delete_button = Button(delete_window, text="Delete record", command=delete_record)
+
+    name_label.grid(row=0, column=0)
+    name_entry.grid(row=0, column=1, columnspan=3)
+    delete_button.grid(row=1, column=0, columnspan=4)
+
 edit = Button(root, text="Create/Edit Record", command=show_edit, width=30)
 search = Button(root, text="Search Record", command=show_search, width=30)
 delete = Button(root, text="Delete Record", command=show_delete, width=30)
